a692eee050524c51e41b751208a5647fc11065d9
grazirs <graziribeiro95@gmail.com>
2021-10-01 10:52:09 -0300

    resolucao atividade 2

--- variaveis-tipos/script.js
+++ variaveis-tipos/script.js
@@ -6,7 +6,7 @@ const é declarada em SNAKE_UPPER_CASE, escopo de bloco, n pode redeclarar, nem 
 var a = 1;
 var b = 2;
 
-if(a === 1){
+if (a === 1) {
     var a = 11; // the scope is global
     let b = 22; // the scope is inside the if-block
     console.log(a); // 11
@@ -19,13 +19,13 @@ console.log(b); // 2
 //hoisting, ele leva a variavel pra cima, mesmo sem ter escrito var, ele consegue ler . Com o let e const nao funciona
 
 numberOne = 1;
-console.log (numberOne + 2);
+console.log(numberOne + 2);
 var numberOne;
 //let numberOne; nessa linha o hoisting nao funciona, pq nao aplica pra let
 
 //exemplo const
 const FIRST_NAME = 'Grazi'
-console.log (FIRST_NAME);
+console.log(FIRST_NAME);
 
 
 /*empty, null e undefined
@@ -36,7 +36,7 @@ undefined nao existe, nao declarou
 
 
 
-Verificar um palindromo: frase ou palavra que pode ler da esquerda pra direita ou vice-versa.*/
+Atividade 1: Verificar um palindromo: frase ou palavra que pode ler da esquerda pra direita ou vice-versa.*/
 
 // solution 1:
 function verificaPalindromo(string) {
@@ -51,10 +51,29 @@ function verificaPalindromo2(string) {
     if (!string) return 'string inexistente';
 
     for (let i = 0; i < string.length / 2; i++) {
-        if(string[i] !== string[string.length -1 -i]) {
+        if (string[i] !== string[string.length - 1 - i]) {
             return false;
         }
     }
     return true;
 }
 console.log(verificaPalindromo2('gato'));
+
+//Atividade 2:
+
+let array = [1, 3, 4, 6, 80, 33, 23, 90];
+function substituiPares(numberArray) {
+    if (!numberArray) return -1;
+    if (!numberArray.length) return -1;
+
+    for (let i = 0; i < numberArray.length; i++) {
+        if (numberArray[i] === 0) {
+            console.log('Você já é zero!');
+        } else if (numberArray[i] % 2 === 0 && numberArray[i] != 0) {
+            console.log(`Substituindo ${numberArray[i]} por 0...`);
+            numberArray[i] = 0;
+        } 
+    }
+    return numberArray;
+    }
+console.log(substituiPares(array));
